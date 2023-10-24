62253dc0e754862d5648a689be51aa319e288e3d
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2023-10-24 19:43:19 +1100

    updated template files

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import htmlPurge from 'vite-plugin-purgecss'
 
 export default defineConfig({
   root: resolve(__dirname, 'src/'),
@@ -9,9 +10,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         home: resolve(__dirname, 'src/index.html'),
-        about: resolve(__dirname, 'src/about/index.html'),
-        contact: resolve(__dirname, 'src/contact/index.html'),
+        // about: resolve(__dirname, 'src/about/index.html'),
+        // contact: resolve(__dirname, 'src/contact/index.html'),
       },
     },
   },
+  plugins: [htmlPurge([htmlPurge()])],
 })
